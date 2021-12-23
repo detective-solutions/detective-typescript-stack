f4cc254c6d4a7bfaf1970e51256b8084e66274f0
phips-git <p.mueller93@gmail.com>
2021-12-23 09:39:24 +0100

    feat(project): add environments library

--- apps/detective-client/src/main.ts
+++ apps/detective-client/src/main.ts
@@ -1,6 +1,6 @@
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from '@detective-frontend/shared/environments';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 if (environment.production) {
