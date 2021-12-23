1452df0705bf9ead71c1e890e1554bd752f347e4
phips-git <p.mueller93@gmail.com>
2021-12-23 10:53:04 +0100

    feat(project): prevent core module imports

--- apps/detective-client/src/app/core.module.ts
+++ apps/detective-client/src/app/core.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { SharedDataAccessModule } from '@detective-frontend/shared/data-access';
 
 @NgModule({
   imports: [SharedDataAccessModule.forRoot()],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        `${parentModule.constructor.name} has already been loaded. Import core modules in the app module only.`
+      );
+    }
+  }
+}
