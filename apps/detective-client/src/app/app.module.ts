d727ac75b41a5edb693b61536b6432941435a852
phips-git <p.mueller93@gmail.com>
2021-12-23 16:55:50 +0100

    feat(project): add Angular Material Design packages

--- apps/detective-client/src/app/app.module.ts
+++ apps/detective-client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
