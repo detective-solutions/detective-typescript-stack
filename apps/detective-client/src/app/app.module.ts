ef1ad4825617068887431cb03b2789f6e8c1f382
phips-git <p.mueller93@gmail.com>
2021-12-28 11:04:02 +0100

    feat(DET-706): add @angular/flex-layout package

--- apps/detective-client/src/app/app.module.ts
+++ apps/detective-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './app/auth/login/login.component';
 import { LogoutComponent } from './app/auth/logout/logout.component';
 import { NgModule } from '@angular/core';
@@ -10,7 +11,7 @@ import { RegisterComponent } from './app/auth/register/register.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent, LogoutComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, FlexLayoutModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
