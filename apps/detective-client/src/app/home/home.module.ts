34d9410f769790127582ccc3c961c753eb99e19c
phips-git <p.mueller93@gmail.com>
2021-12-28 17:58:02 +0100

    feat(DET-706): remove redundant HomeModule

--- apps/detective-client/src/app/home/home.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { HomeRoutingModule } from './home-routing.module';
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, HomeRoutingModule],
-})
-export class HomeModule {}
