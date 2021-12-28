cba24d64a47b0c8a0eb9e0354e1087e01b15ebdd
phips-git <p.mueller93@gmail.com>
2021-12-28 11:14:34 +0100

    refactor(DET-706): use inline template for AppComponent

--- apps/detective-client/src/app/app.component.ts
+++ apps/detective-client/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'detective-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
   title = 'detective-client';
