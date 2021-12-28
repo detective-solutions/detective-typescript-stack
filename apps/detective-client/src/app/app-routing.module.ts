c2e515fd90a051923fd7d0997880af3eab6b4d0d
phips-git <p.mueller93@gmail.com>
2021-12-28 18:00:10 +0100

    feat(DET-706): add register router to RegisterComponent

--- apps/detective-client/src/app/app-routing.module.ts
+++ apps/detective-client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from '@detective.solutions/shared/components';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/portal/my-casefiles', pathMatch: 'full' },
   { path: 'portal', loadChildren: () => import('./portal/portal.module').then((m) => m.PortalModule) },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
