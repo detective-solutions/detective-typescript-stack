1a5af701d54592f83231c966df4f8ba0520b2a31
phips-git <p.mueller93@gmail.com>
2021-12-28 11:02:50 +0100

    ci(project): allow JIRA tickets in commit scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,1 @@
-module.exports = { extends: ['@commitlint/config-angular'], rules: { 'scope-empty': [2, 'never'] } };
+module.exports = { extends: ['@commitlint/config-angular'], rules: { 'scope-empty': [2, 'never'], 'scope-case': [0] } };
