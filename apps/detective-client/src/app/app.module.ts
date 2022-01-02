c674e144d0bd53d6345d47fe9e3d89bdf5a8f802
phips-git <p.mueller93@gmail.com>
2022-01-02 18:02:55 +0100

    refactor(DET-706): remove FlexLayoutModule from AppModule

--- apps/detective-client/src/app/app.module.ts
+++ apps/detective-client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './auth/login/login.component';
 import { LogoutComponent } from './auth/logout/logout.component';
 import { NgModule } from '@angular/core';
@@ -12,7 +11,7 @@ import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent, LogoutComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, FlexLayoutModule, AppMaterialModule],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
