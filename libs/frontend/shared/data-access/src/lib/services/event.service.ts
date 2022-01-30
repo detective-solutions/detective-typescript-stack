9cdfa66bad7a5cb10f300fb8d95a504e8a322c31
phips-git <p.mueller93@gmail.com>
2022-01-30 14:41:48 +0100

    feat(DET-768): show tiles per default

--- libs/frontend/shared/data-access/src/lib/services/event.service.ts
+++ libs/frontend/shared/data-access/src/lib/services/event.service.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class EventService {
-  showTableView$ = new BehaviorSubject<boolean>(true);
+  showTableView$ = new BehaviorSubject<boolean>(false);
 }
