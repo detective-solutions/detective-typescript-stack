5f74c63a0d7bc03f629314fba0bcfa4853cab03c
phips-git <p.mueller93@gmail.com>
2022-01-30 14:45:01 +0100

    feat(PT-768): improve tile sizing

--- libs/frontend/detective-client/ui/src/lib/tiles/tiles.material.module.ts
+++ libs/frontend/detective-client/ui/src/lib/tiles/tiles.material.module.ts
@@ -2,9 +2,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 
-const modules = [MatCardModule, MatButtonModule, MatIconModule, FlexLayoutModule];
+const modules = [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule, FlexLayoutModule];
 
 @NgModule({
   exports: modules,
