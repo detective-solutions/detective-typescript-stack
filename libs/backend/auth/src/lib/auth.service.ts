03793111f9bd10673681e3f9da0dc150d0995baa
phips-git <p.mueller93@gmail.com>
2022-01-31 08:01:14 +0100

    build(DET-766): disable eslint rules

--- libs/backend/auth/src/lib/auth.service.ts
+++ libs/backend/auth/src/lib/auth.service.ts
@@ -5,9 +5,11 @@ import { UsersService } from '@detective.solutions/backend/users';
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
     if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
