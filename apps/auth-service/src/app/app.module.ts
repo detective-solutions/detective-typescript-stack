506fb58156f8e8a12b9a64377383420e77402777
phips-git <p.mueller93@gmail.com>
2022-01-31 17:44:50 +0100

    feat(DET-766): import AuthModule in AppModule

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { AppController } from './app.controller';
+import { AuthModule } from '@detective.solutions/backend/auth';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AppController],
 })
 export class AppModule {}
