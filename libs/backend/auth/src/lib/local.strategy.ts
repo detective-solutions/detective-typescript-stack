e193cb7f18bc484ea7bdb00ca01a504941a3d0fd
phips-git <p.mueller93@gmail.com>
2022-01-31 08:00:18 +0100

    refactor(PT-766): clean up code & rename vars

--- libs/backend/auth/src/lib/local.strategy.ts
+++ libs/backend/auth/src/lib/local.strategy.ts
@@ -6,12 +6,12 @@ import { Strategy } from 'passport-local';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private readonly authService: AuthService) {
     super({ usernameField: 'email' });
   }
 
-  async validate(username: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(username, password);
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException();
     }
