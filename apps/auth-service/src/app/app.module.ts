f0d11a3dad5fa7f774d06ce1f22f3473e2a2a85b
phips-git <p.mueller93@gmail.com>
2022-02-01 08:07:14 +0100

    feat(DET-766): add AuthService provider to AppModule

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { AuthModule, AuthService } from '@detective.solutions/backend/auth';
+
 import { AppController } from './app.controller';
-import { AuthModule } from '@detective.solutions/backend/auth';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [AuthModule],
+  providers: [AuthService],
   controllers: [AppController],
 })
 export class AppModule {}
