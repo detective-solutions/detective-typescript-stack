46e66e9dca9bfaad593bb9ad6636e2c04d6bc7c0
phips-git <p.mueller93@gmail.com>
2022-02-01 08:04:34 +0100

    test(DET-766): add AuthService tests

--- libs/backend/auth/src/lib/auth.service.spec.ts
+++ libs/backend/auth/src/lib/auth.service.spec.ts
@@ -1,19 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
 import { AuthService } from './auth.service';
-import { Test } from '@nestjs/testing';
+import { IUser } from '@detective.solutions/shared/data-access';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersService } from '@detective.solutions/backend/users';
 
 describe('AuthService', () => {
+  const testUser: IUser = { email: 'tester@detective.solutions', password: 'test' };
   let service: AuthService;
 
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [AuthService, UsersService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          secret: 'testSecret',
+          signOptions: { expiresIn: '30s' },
+        }),
+      ],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn(() => testUser),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get(AuthService);
+    service = moduleRef.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeDefined();
+  });
+
+  it('should return a user object when validateUser is called with valid credentials', async () => {
+    const res = await service.validateUser('tester@detective.solutions', 'test');
+    expect(res.email).toEqual('tester@detective.solutions');
+  });
+
+  it('should not return the password when validateUser is called with valid credentials', async () => {
+    const res = await service.validateUser('tester@detective.solutions', 'test');
+    expect(res.password).toBeFalsy();
+  });
+
+  it('should return null when validateUser is called with invalid credentials', async () => {
+    const res = await service.validateUser('xxx', 'xxx');
+    expect(res).toBeNull();
+  });
+
+  it('should return a JWT object when login is called with valid credentials', async () => {
+    const res = await service.login({ email: 'tester@detective.solutions', password: 'test' });
+    expect(res.access_token).toBeDefined();
   });
 });
