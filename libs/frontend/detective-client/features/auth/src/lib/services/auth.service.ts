f7cfef5182592ede835d2244b553dbcfa2122194
phips-git <p.mueller93@gmail.com>
2022-02-05 15:43:27 +0100

    feat(DET-766): add abstract methods to the AuthService

--- libs/frontend/detective-client/features/auth/src/lib/services/auth.service.ts
+++ libs/frontend/detective-client/features/auth/src/lib/services/auth.service.ts
@@ -1,23 +1,45 @@
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, filter, map, mergeMap, tap, throwError } from 'rxjs';
 import { IAuthStatus, defaultAuthStatus } from '../interfaces/auth-status.interface';
 
+import { IAuthServerResponse } from '../interfaces/auth-server-response.interface';
 import { IAuthService } from '../interfaces/auth-service.interface';
 import { IUser } from '@detective.solutions/shared/data-access';
 import { Injectable } from '@angular/core';
 import { User } from '@detective.solutions/frontend/shared/data-access';
+import jwtDecode from 'jwt-decode';
+import { transformError } from '@detective.solutions/frontend/shared/utils';
 
 @Injectable()
 export abstract class AuthService implements IAuthService {
   readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
   readonly currentUser$ = new BehaviorSubject<IUser>(new User());
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected abstract authProvider(email: string, password: string): Observable<IAuthServerResponse>;
+  protected abstract transformJwtToken(token: unknown): IAuthStatus;
+  protected abstract getCurrentUser(): Observable<User>;
+
   login(email: string, password: string): Observable<void> {
-    return new Observable<void>();
+    const loginResponse$ = this.authProvider(email, password).pipe(
+      map((value) => this.transformJwtToken(jwtDecode(value.accessToken))),
+      tap((status) => this.authStatus$.next(status)),
+      filter((status: IAuthStatus) => status.isAuthenticated),
+      mergeMap(() => this.getCurrentUser()),
+      map((user: IUser) => this.currentUser$.next(user)),
+      catchError(transformError)
+    );
+    loginResponse$.subscribe({
+      error: (err) => {
+        this.logout();
+        return throwError(() => new Error(err));
+      },
+    });
+    return loginResponse$;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  logout() {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  logout(clearToken?: boolean) {
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0);
+  }
 
   getToken() {
     return '';
