3bf48c1e42b23cf1fda23bf778771ac030a3f914
phips-git <p.mueller93@gmail.com>
2022-02-05 16:03:30 +0100

    feat(DET-766): add abstract CacheService

--- libs/frontend/detective-client/features/auth/src/lib/services/auth.service.ts
+++ libs/frontend/detective-client/features/auth/src/lib/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { BehaviorSubject, Observable, catchError, filter, map, mergeMap, tap, throwError } from 'rxjs';
 import { IAuthStatus, defaultAuthStatus } from '../interfaces/auth-status.interface';
 
+import { CacheService } from './cache.service';
 import { IAuthServerResponse } from '../interfaces/auth-server-response.interface';
 import { IAuthService } from '../interfaces/auth-service.interface';
 import { IUser } from '@detective.solutions/shared/data-access';
@@ -10,17 +11,26 @@ import jwtDecode from 'jwt-decode';
 import { transformError } from '@detective.solutions/frontend/shared/utils';
 
 @Injectable()
-export abstract class AuthService implements IAuthService {
+export abstract class AuthService extends CacheService implements IAuthService {
   readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
   readonly currentUser$ = new BehaviorSubject<IUser>(new User());
+
+  constructor() {
+    super();
+  }
 
   protected abstract authProvider(email: string, password: string): Observable<IAuthServerResponse>;
   protected abstract transformJwtToken(token: unknown): IAuthStatus;
   protected abstract getCurrentUser(): Observable<User>;
 
   login(email: string, password: string): Observable<void> {
+    this.clearToken();
+
     const loginResponse$ = this.authProvider(email, password).pipe(
-      map((value) => this.transformJwtToken(jwtDecode(value.accessToken))),
+      map((value) => {
+        this.setToken(value.accessToken);
+        return this.transformJwtToken(jwtDecode(value.accessToken));
+      }),
       tap((status) => this.authStatus$.next(status)),
       filter((status: IAuthStatus) => status.isAuthenticated),
       mergeMap(() => this.getCurrentUser()),
@@ -36,12 +46,22 @@ export abstract class AuthService implements IAuthService {
     return loginResponse$;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   logout(clearToken?: boolean) {
+    if (clearToken) {
+      this.clearToken();
+    }
     setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0);
   }
 
-  getToken() {
-    return '';
+  protected setToken(jwt: string) {
+    this.setItem('jwt', jwt);
+  }
+
+  getToken(): string {
+    return this.getItem('jwt') ?? '';
+  }
+
+  protected clearToken() {
+    this.removeItem('jwt');
   }
 }
