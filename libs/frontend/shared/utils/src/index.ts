266c6389efde53c3dcac97c9cbe0070ec0aa75f5
phips-git <p.mueller93@gmail.com>
2022-02-06 15:19:49 +0100

    feat(DET-766): add reusable form validators

--- libs/frontend/shared/utils/src/index.ts
+++ libs/frontend/shared/utils/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/form-validators';
 export * from './lib/transform-error-util';
---
+++ libs/frontend/shared/utils/src/lib/form-validators.ts
@@ -0,0 +1,7 @@
+import { Validators } from '@angular/forms';
+
+export const OptionalTextValidation = [Validators.minLength(2), Validators.maxLength(50)];
+export const RequiredTextValidation = OptionalTextValidation.concat([Validators.required]);
+export const OneCharValidation = [Validators.minLength(1), Validators.maxLength(1)];
+export const EmailValidation = [Validators.required, Validators.email];
+export const PasswordValidation = [Validators.required, Validators.minLength(8), Validators.maxLength(50)];
