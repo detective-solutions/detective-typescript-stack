6024bf30c347697703c12084b04cd35c1f78eb1d
phips-git <p.mueller93@gmail.com>
2022-02-06 12:42:06 +0100

    feat(DET-766): add AuthHttpInterceptor

--- apps/detective-client/src/app/app.module.ts
+++ apps/detective-client/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { AuthModule, AuthService, InMemoryAuthService } from '@detective.solutions/detective-client/features/auth';
+import {
+  AuthHttpInterceptor,
+  AuthModule,
+  AuthService,
+  InMemoryAuthService,
+} from '@detective.solutions/detective-client/features/auth';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, TranslocoRootModule, AuthModule],
-  providers: [{ provide: AuthService, useClass: InMemoryAuthService }],
+  providers: [
+    { provide: AuthService, useClass: InMemoryAuthService },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/frontend/detective-client/features/auth/src/index.ts
+++ libs/frontend/detective-client/features/auth/src/index.ts
@@ -3,3 +3,4 @@ export { LoginComponent } from './lib/components/login/login.component';
 export { RegisterComponent } from './lib/components/register/register.component';
 export * from './lib/services/auth.service';
 export * from './lib/services/auth.inmemory.service';
+export * from './lib/interceptors/auth-http-interceptor';
