ef6cfab26c9fba4668682058f76015c0f42d4bfd
phips-git <p.mueller93@gmail.com>
2022-02-06 15:37:10 +0100

    feat(DET-766): adjust shared form validators

--- libs/frontend/shared/utils/src/lib/form-validators.ts
+++ libs/frontend/shared/utils/src/lib/form-validators.ts
@@ -3,5 +3,5 @@ import { Validators } from '@angular/forms';
 export const OptionalTextValidation = [Validators.minLength(2), Validators.maxLength(50)];
 export const RequiredTextValidation = OptionalTextValidation.concat([Validators.required]);
 export const OneCharValidation = [Validators.minLength(1), Validators.maxLength(1)];
-export const EmailValidation = [Validators.required, Validators.email];
-export const PasswordValidation = [Validators.required, Validators.minLength(8), Validators.maxLength(50)];
+export const EmailValidation = [Validators.required, Validators.email, Validators.maxLength(254)];
+export const PasswordValidation = [Validators.required, Validators.minLength(8), Validators.maxLength(64)];
