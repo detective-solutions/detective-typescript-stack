8032b55081012e6b4309fc83c0bf5ea320696be1
phips-git <p.mueller93@gmail.com>
2022-02-11 23:05:49 +0100

    fix(DET-766): rename exported default config object

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { AuthModule, AuthService } from '@detective.solutions/backend/auth';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { defaultServerConfig } from './app-default.confg';
+import { defaultConfig } from './default.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      validationSchema: defaultServerConfig,
+      validationSchema: defaultConfig,
     }),
     AuthModule,
   ],
