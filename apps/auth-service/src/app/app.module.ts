073f4b8bf97ef53a8f963c4041a7d2f627cb7cdd
phips-git <p.mueller93@gmail.com>
2022-02-11 23:39:34 +0100

    refactor(project): rename default env config file

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { AuthModule, AuthService } from '@detective.solutions/backend/auth';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { defaultConfig } from './default.config';
+import { defaultEnvConfig } from './default-env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      validationSchema: defaultConfig,
+      validationSchema: defaultEnvConfig,
     }),
     AuthModule,
   ],
--- apps/auth-service/src/app/default-env.config.ts
+++ apps/auth-service/src/app/default-env.config.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 
-export const defaultConfig = Joi.object({
+export const defaultEnvConfig = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production').default('development'),
   PORT: Joi.number().default(1111),
   ACCESS_TOKEN_SECRET: Joi.string().required(),
