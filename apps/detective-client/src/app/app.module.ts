030b31664e27c604c193fc027953b5d9c21922bd
phips-git <p.mueller93@gmail.com>
2022-03-06 16:30:54 +0100

    feat(DET-766): correctly import updated modules in AppModule

--- apps/detective-client/src/app/app.module.ts
+++ apps/detective-client/src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { AuthHttpInterceptor, AuthModule, AuthService } from '@detective.solutions/detective-client/features/auth';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from '@detective.solutions/detective-client/features/auth';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core.module';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
 import { NgModule } from '@angular/core';
+import { SharedErrorHandlingModule } from '@detective.solutions/frontend/shared/error-handling';
+import { SharedUiModule } from '@detective.solutions/frontend/shared/ui';
 import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, BrowserAnimationsModule, AuthModule, TranslocoRootModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    TranslocoRootModule,
+    AuthModule,
+    SharedUiModule,
+    BrowserAnimationsModule,
+    SharedErrorHandlingModule, // Import last due to interceptor chain
+  ],
   providers: [
     {
       provide: APOLLO_OPTIONS,
