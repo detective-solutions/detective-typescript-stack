1095f61b75461ec579892aee7c573e1e326fd9ea
phips-git <p.mueller93@gmail.com>
2022-03-06 16:32:34 +0100

    build(project): set production log level

--- libs/frontend/shared/environments/src/lib/environment.prod.ts
+++ libs/frontend/shared/environments/src/lib/environment.prod.ts
@@ -1,5 +1,8 @@
+import { LogLevel } from '@detective.solutions/frontend/shared/error-handling';
+
 export const environment = {
   production: true,
   baseUrl: '',
   authMode: 'custom', // Cannot use AuthMode enum due to circular dependency
+  logLevel: LogLevel.Info,
 };
