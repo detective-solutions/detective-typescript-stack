7e32d9760a3858af8224a70fe37a649864731cc0
phips-git <p.mueller93@gmail.com>
2022-03-17 19:53:03 +0100

    feat(DET-784): add & update data source icons

--- libs/frontend/shared/data-access/src/lib/dto/data-source.dto.ts
+++ libs/frontend/shared/data-access/src/lib/dto/data-source.dto.ts
@@ -1,20 +1,28 @@
 import { IDataSource } from '@detective.solutions/shared/data-access';
 
 export enum DataSourceType {
-  SQL = 'mysql',
-  POSTGRES = 'postgresql',
   EXCEL = 'excel',
   PDF = 'pdf',
-  AZURE_BLOB = 'azureBlob',
-  AWS_BLOB = 'awsBlob',
+  POSTGRES = 'postgresql',
+  MICROSOFT_SQL = 'mssql',
+  MY_SQL = 'mysql',
+  AZURE_SQL = 'azure-sql',
+  AZURE_BLOB = 'azure-blob',
+  AWS_BLOB = 'aws-blob',
+  AWS_DYNAMODB = 'aws-dynamo',
 }
 
 export class DataSource implements IDataSource {
-  static readonly sqlThumbnail = 'assets/icons/data-sources/sql.svg';
   static readonly excelThumbnail = 'assets/icons/data-sources/excel.svg';
   static readonly pdfThumbnail = 'assets/icons/data-sources/pdf.svg';
-  static readonly azureBlobThumbnail = 'assets/icons/data-sources/azureBlob.svg';
-  static readonly awsBlobThumbnail = 'assets/icons/data-sources/awsBlob.svg';
+  static readonly postgresThumbnail = 'assets/icons/data-sources/postgres.svg';
+  static readonly microsoftSqlThumbnail = 'assets/icons/data-sources/ms-sql.svg';
+  static readonly mySqlThumbnail = 'assets/icons/data-sources/my-sql.svg';
+  static readonly azureSqlThumbnail = 'assets/icons/data-sources/azure-sql.svg';
+  static readonly azureBlobThumbnail = 'assets/icons/data-sources/azure-blob.svg';
+  static readonly awsBlobThumbnail = 'assets/icons/data-sources/aws-blob.svg';
+  static readonly awsDynamoDbThumbnail = 'assets/icons/data-sources/aws-dynamodb.svg';
+  static readonly defaultThumbnail = 'assets/icons/data-sources/default.svg';
 
   constructor(
     public id = '',
@@ -33,30 +41,42 @@ export class DataSource implements IDataSource {
     dataSource = Object.assign({ iconSrc: '' }, { ...dataSource });
 
     switch (dataSource.db_type) {
-      case DataSourceType.SQL:
-        dataSource.iconSrc = DataSource.sqlThumbnail;
-        break;
-      case DataSourceType.POSTGRES:
-        dataSource.iconSrc = DataSource.sqlThumbnail;
-        break;
       case DataSourceType.EXCEL:
         dataSource.iconSrc = DataSource.excelThumbnail;
         break;
       case DataSourceType.PDF:
         dataSource.iconSrc = DataSource.pdfThumbnail;
         break;
+      case DataSourceType.POSTGRES:
+        dataSource.iconSrc = DataSource.postgresThumbnail;
+        break;
+      case DataSourceType.MICROSOFT_SQL:
+        dataSource.iconSrc = DataSource.microsoftSqlThumbnail;
+        break;
+      case DataSourceType.MY_SQL:
+        dataSource.iconSrc = DataSource.mySqlThumbnail;
+        break;
+      case DataSourceType.AZURE_SQL:
+        dataSource.iconSrc = DataSource.azureSqlThumbnail;
+        break;
       case DataSourceType.AZURE_BLOB:
         dataSource.iconSrc = DataSource.azureBlobThumbnail;
         break;
       case DataSourceType.AWS_BLOB:
         dataSource.iconSrc = DataSource.awsBlobThumbnail;
+        break;
+      case DataSourceType.AWS_DYNAMODB:
+        dataSource.iconSrc = DataSource.awsDynamoDbThumbnail;
+        break;
+      default:
+        dataSource.iconSrc = DataSource.defaultThumbnail;
     }
 
     return new DataSource(
       dataSource.id,
-      dataSource.name ?? 'Test',
+      dataSource.name,
       dataSource.db_type,
-      dataSource.description ?? 'Test',
+      dataSource.description,
       dataSource.iconSrc,
       (dataSource.lastUpdated as Date) ?? new Date()
     );
