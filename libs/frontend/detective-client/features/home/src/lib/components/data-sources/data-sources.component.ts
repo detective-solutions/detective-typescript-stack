bf01b058136fd2998eaab03507929cbb05b2f9ad
phips-git <p.mueller93@gmail.com>
2022-03-18 09:01:57 +0100

    feat(DET-784): subscribe to access events for data sources

--- libs/frontend/detective-client/features/home/src/lib/components/data-sources/data-sources.component.ts
+++ libs/frontend/detective-client/features/home/src/lib/components/data-sources/data-sources.component.ts
@@ -1,6 +1,7 @@
 import { AccessState, ITableInput, TableCellTypes } from '@detective.solutions/frontend/detective-client/ui';
+import { CasefileEventType, EventService, ICasefileEvent } from '@detective.solutions/frontend/shared/data-access';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, Subscription, map } from 'rxjs';
+import { Observable, Subject, Subscription, filter, map, tap } from 'rxjs';
 
 import { DataSourceService } from '../../services/data-source.service';
 import { IDataSource } from '@detective.solutions/shared/data-access';
@@ -24,7 +25,17 @@ export class DataSourcesComponent implements OnInit, OnDestroy {
   private readonly initialPageOffset = 0;
   private readonly subscriptions = new Subscription();
 
-  constructor(private dataSourceService: DataSourceService) {}
+  readonly accessRequested$ = this.subscriptions.add(
+    this.eventService.tableCellEvents$
+      .pipe(
+        tap(console.log),
+        filter((event: ICasefileEvent) => event.type === CasefileEventType.REQUEST_ACCESS && event.value !== undefined),
+        tap((event: ICasefileEvent) => console.log(event))
+      )
+      .subscribe()
+  );
+
+  constructor(private readonly dataSourceService: DataSourceService, private readonly eventService: EventService) {}
 
   ngOnInit() {
     this.dataSources$ = this.dataSourceService.getAllDataSources(this.initialPageOffset, this.pageSize);
