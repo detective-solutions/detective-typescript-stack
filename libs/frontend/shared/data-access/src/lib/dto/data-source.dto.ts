d3d5be2d53ba920834e4fd4e13bd69ba5288d5cf
phips-git <p.mueller93@gmail.com>
2022-03-18 08:58:42 +0100

    feat(DET-784): add additional data source icons

--- libs/frontend/shared/data-access/src/lib/dto/data-source.dto.ts
+++ libs/frontend/shared/data-access/src/lib/dto/data-source.dto.ts
@@ -3,9 +3,12 @@ import { IDataSource } from '@detective.solutions/shared/data-access';
 export enum DataSourceType {
   EXCEL = 'excel',
   PDF = 'pdf',
-  POSTGRES = 'postgresql',
-  MICROSOFT_SQL = 'mssql',
+  MONGO_DB = 'mongodb',
   MY_SQL = 'mysql',
+  MARIA_DB = 'mariadb',
+  POSTGRES = 'postgresql',
+  ORACLE_DB = 'oracle',
+  MICROSOFT_SQL = 'mssql',
   AZURE_SQL = 'azure-sql',
   AZURE_BLOB = 'azure-blob',
   AWS_BLOB = 'aws-blob',
@@ -15,9 +18,12 @@ export enum DataSourceType {
 export class DataSource implements IDataSource {
   static readonly excelThumbnail = 'assets/icons/data-sources/excel.svg';
   static readonly pdfThumbnail = 'assets/icons/data-sources/pdf.svg';
-  static readonly postgresThumbnail = 'assets/icons/data-sources/postgres.svg';
-  static readonly microsoftSqlThumbnail = 'assets/icons/data-sources/ms-sql.svg';
+  static readonly mongoDbThumbnail = 'assets/icons/data-sources/mongo-db.svg';
   static readonly mySqlThumbnail = 'assets/icons/data-sources/my-sql.svg';
+  static readonly mariaDbThumbnail = 'assets/icons/data-sources/maria-db.svg';
+  static readonly postgresThumbnail = 'assets/icons/data-sources/postgres.svg';
+  static readonly oracleDbThumbnail = 'assets/icons/data-sources/oracle-db.svg';
+  static readonly microsoftSqlThumbnail = 'assets/icons/data-sources/ms-sql.svg';
   static readonly azureSqlThumbnail = 'assets/icons/data-sources/azure-sql.svg';
   static readonly azureBlobThumbnail = 'assets/icons/data-sources/azure-blob.svg';
   static readonly awsBlobThumbnail = 'assets/icons/data-sources/aws-blob.svg';
@@ -38,48 +44,45 @@ export class DataSource implements IDataSource {
       return new DataSource();
     }
 
-    dataSource = Object.assign({ iconSrc: '' }, { ...dataSource });
-
-    switch (dataSource.db_type) {
-      case DataSourceType.EXCEL:
-        dataSource.iconSrc = DataSource.excelThumbnail;
-        break;
-      case DataSourceType.PDF:
-        dataSource.iconSrc = DataSource.pdfThumbnail;
-        break;
-      case DataSourceType.POSTGRES:
-        dataSource.iconSrc = DataSource.postgresThumbnail;
-        break;
-      case DataSourceType.MICROSOFT_SQL:
-        dataSource.iconSrc = DataSource.microsoftSqlThumbnail;
-        break;
-      case DataSourceType.MY_SQL:
-        dataSource.iconSrc = DataSource.mySqlThumbnail;
-        break;
-      case DataSourceType.AZURE_SQL:
-        dataSource.iconSrc = DataSource.azureSqlThumbnail;
-        break;
-      case DataSourceType.AZURE_BLOB:
-        dataSource.iconSrc = DataSource.azureBlobThumbnail;
-        break;
-      case DataSourceType.AWS_BLOB:
-        dataSource.iconSrc = DataSource.awsBlobThumbnail;
-        break;
-      case DataSourceType.AWS_DYNAMODB:
-        dataSource.iconSrc = DataSource.awsDynamoDbThumbnail;
-        break;
-      default:
-        dataSource.iconSrc = DataSource.defaultThumbnail;
-    }
-
     return new DataSource(
       dataSource.id,
       dataSource.name,
       dataSource.db_type,
       dataSource.description,
-      dataSource.iconSrc,
+      dataSource.iconSrc ?? DataSource.getDataSourceIconSrc(dataSource.db_type),
       (dataSource.lastUpdated as Date) ?? new Date()
     );
+  }
+
+  private static getDataSourceIconSrc(dataSourceType: string): string {
+    switch (dataSourceType) {
+      case DataSourceType.EXCEL:
+        return DataSource.excelThumbnail;
+      case DataSourceType.PDF:
+        return DataSource.pdfThumbnail;
+      case DataSourceType.MONGO_DB:
+        return DataSource.mongoDbThumbnail;
+      case DataSourceType.MY_SQL:
+        return DataSource.mySqlThumbnail;
+      case DataSourceType.MARIA_DB:
+        return DataSource.mariaDbThumbnail;
+      case DataSourceType.POSTGRES:
+        return DataSource.postgresThumbnail;
+      case DataSourceType.ORACLE_DB:
+        return DataSource.oracleDbThumbnail;
+      case DataSourceType.MICROSOFT_SQL:
+        return DataSource.microsoftSqlThumbnail;
+      case DataSourceType.AZURE_SQL:
+        return DataSource.azureSqlThumbnail;
+      case DataSourceType.AZURE_BLOB:
+        return DataSource.azureBlobThumbnail;
+      case DataSourceType.AWS_BLOB:
+        return DataSource.awsBlobThumbnail;
+      case DataSourceType.AWS_DYNAMODB:
+        return DataSource.awsDynamoDbThumbnail;
+      default:
+        return DataSource.defaultThumbnail;
+    }
   }
 
   toJSON(): object {
