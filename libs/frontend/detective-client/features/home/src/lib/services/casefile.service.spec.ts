e047f8a030128dd50f0fa958cc9114a33627e102
phips-git <p.mueller93@gmail.com>
2022-03-22 22:14:02 +0100

    refactor(DET-771): introduce mockUtils for additional tests

--- libs/frontend/detective-client/features/home/src/lib/services/casefile.service.spec.ts
+++ libs/frontend/detective-client/features/home/src/lib/services/casefile.service.spec.ts
@@ -1,9 +1,23 @@
+import { Subject, of } from 'rxjs';
+
 import { Apollo } from 'apollo-angular';
 import { CasefileService } from './casefile.service';
 import { EventService } from '@detective.solutions/frontend/shared/data-access';
 import { GetAllCasefilesGQL } from '../graphql/get-all-casefiles-gql';
 import { GetCasefilesByAuthorGQL } from '../graphql/get-casefiles-by-author.gql';
 import { TestBed } from '@angular/core/testing';
+
+const mockUtils = {
+  eventServiceMock: {
+    resetLoadingStates$: new Subject(),
+  },
+  createQueryRefMock: (mockResponse: object) => {
+    return {
+      valueChanges: of({ data: mockResponse, loading: false, networkStatus: 7 }),
+      fetchMore: jest.fn().mockReturnValue(new Promise(jest.fn())),
+    };
+  },
+};
 
 describe('CasefileService', () => {
   let service: CasefileService;
