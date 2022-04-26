fd59d0ebef0f9aee5b2d58e57b38ab6266a7ae38
phips-git <p.mueller93@gmail.com>
2022-04-26 07:52:32 +0200

    feat(DET-843): add basic Kafka client to AppService

--- apps/websocket-gateway/src/app/app.service.ts
+++ apps/websocket-gateway/src/app/app.service.ts
@@ -1,7 +1,17 @@
+import { Client, ClientKafka } from '@nestjs/microservices';
+
 import { Injectable } from '@nestjs/common';
+import { microserviceConfig } from './microservice-config';
 
 @Injectable()
 export class AppService {
+  @Client(microserviceConfig)
+  client: ClientKafka;
+
+  sendTestMessage() {
+    this.client.emit<string>('test1', 'some entity ' + new Date());
+  }
+
   getData(): { message: string } {
     return { message: 'Welcome to websocket-gateway!' };
   }
