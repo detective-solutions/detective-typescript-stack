9f1a70e394c14782646c1336dd54b77e217ad634
phips-git <p.mueller93@gmail.com>
2022-05-31 09:21:47 +0200

    refactor(DET-843): remove unnecessary files

--- apps/websocket-gateway/src/app/kafka-adapter/kafka-adapter.module.ts
+++
@@ -1,6 +0,0 @@
-import { AuthModule } from '@detective.solutions/backend/auth';
-import { KafkaAdapterGateway } from './kafka-adapter.gateway';
-import { Module } from '@nestjs/common';
-
-@Module({ imports: [AuthModule], providers: [KafkaAdapterGateway] })
-export class KafkaAdapterModule {}
--- apps/websocket-gateway/src/app/mouse-mirror/mouse-mirror.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MouseMirrorGateway } from './mouse-mirror.gateway';
-
-@Module({ providers: [MouseMirrorGateway] })
-export class MouseMirrorModule {}
