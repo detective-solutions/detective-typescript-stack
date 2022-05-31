32cfacba6cd0f781a7ec147dbebe729f1219b246
phips-git <p.mueller93@gmail.com>
2022-05-31 09:21:10 +0200

    feat(DET-843): add websocket gateway app controller

--- apps/websocket-gateway/src/app/app.controller.ts
+++ apps/websocket-gateway/src/app/app.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
+import { WhiteboardDataGateway } from './websocket-gateways';
 
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly whiteboardDataGateway: WhiteboardDataGateway) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @EventPattern('casefile')
+  forwardQueryExecution(data: any) {
+    console.log('FORWARD EVENT WITH DATA', data.value);
+    this.whiteboardDataGateway.broadcastMessage(JSON.stringify(data.value));
   }
 }
