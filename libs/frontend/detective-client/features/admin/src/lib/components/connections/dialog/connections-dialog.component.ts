8d0d4dce794d55e2fc4b467fc61bcc32d1326967
phips-git <p.mueller93@gmail.com>
2022-06-17 17:26:41 +0200

    feat(DET-860): enhance ConnectionsDialogComponent

--- libs/frontend/detective-client/features/admin/src/lib/components/connections/dialog/connections-dialog.component.ts
+++ libs/frontend/detective-client/features/admin/src/lib/components/connections/dialog/connections-dialog.component.ts
@@ -1,11 +1,6 @@
-import {
-  BaseFormField,
-  CheckboxFormField,
-  DropdownFormField,
-  TextBoxFormField,
-} from '@detective.solutions/frontend/shared/dynamic-form';
+import { CheckboxFormField, TextBoxFormField } from '@detective.solutions/frontend/shared/dynamic-form';
 import { Component, Inject } from '@angular/core';
-import { Observable, map, of, switchMap, tap } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 import { ConnectionsService } from '../../../services';
 import { FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -21,30 +16,11 @@ export class ConnectionsDialogComponent {
   connectorTypeFormGroup = this.formBuilder.group({ connectorType: 'connectorType' });
 
   availableConnectorTypes$ = this.connectionsService.getAvailableConnectorTypes();
+
   formFieldDefinitionsByConnectorType = this.connectorTypeFormGroup.get('connectorType')?.valueChanges.pipe(
-    tap(console.log),
     switchMap((selectedConnectorType: string) => this.connectionsService.getConnectorProperties(selectedConnectorType)),
-    map(this.getFormFieldByType),
-    tap(console.log)
+    map(this.getFormFieldByType)
   );
-  formFieldDefinitions$: Observable<BaseFormField<any>[]> = of([
-    new DropdownFormField({
-      key: 'connectorTypes',
-      label: 'Connector Type',
-      options: [
-        { key: 'accumulo', value: 'Accumulo' },
-        { key: 'bigquery', value: 'BigQuery' },
-        { key: 'cassandra', value: 'Cassandra' },
-        { key: 'hive', value: 'Hive' },
-        { key: 'postgresql', value: 'PostgreSQL' },
-      ],
-    }),
-    new TextBoxFormField({
-      key: 'connectorName',
-      label: 'Name',
-      required: true,
-    }),
-  ]);
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: object,
@@ -57,23 +33,37 @@ export class ConnectionsDialogComponent {
     Object.values(formFieldData.properties).forEach((data: any) => {
       switch (data.type) {
         case 'boolean': {
-          formFieldDefinition.push(new CheckboxFormField({ key: data.title, label: data.title, type: 'checkbox' }));
+          formFieldDefinition.push(
+            new CheckboxFormField({ key: data.title, label: data.title, type: 'checkbox', value: data.default })
+          );
           return;
         }
         case 'string': {
           formFieldDefinition.push(
-            new TextBoxFormField({ key: data.title, label: data.title, type: 'text', required: true })
+            new TextBoxFormField({
+              key: data.title,
+              label: data.title,
+              type: 'text',
+              value: data.default,
+              required: true,
+            })
           );
           return;
         }
         case 'integer': {
           formFieldDefinition.push(
-            new TextBoxFormField({ key: data.title, label: data.title, type: 'number', required: true })
+            new TextBoxFormField({
+              key: data.title,
+              label: data.title,
+              type: 'number',
+              value: data.default,
+              required: true,
+            })
           );
           return;
         }
         default: {
-          return;
+          throw new Error(`Unknown connector property type ${data?.type}`);
         }
       }
     });
