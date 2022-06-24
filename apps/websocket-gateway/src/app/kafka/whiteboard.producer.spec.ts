c021bf1dfa92bbe3d00bdf4618cc0117d1027a38
phips-git <p.mueller93@gmail.com>
2022-06-24 10:23:43 +0200

    test(DET-861): add WhiteboardProducer unit test

--- apps/websocket-gateway/src/app/kafka/whiteboard.producer.spec.ts
+++ apps/websocket-gateway/src/app/kafka/whiteboard.producer.spec.ts
@@ -1,16 +1,27 @@
 import { ClientKafka } from '@nestjs/microservices';
 import { Test } from '@nestjs/testing';
 import { WhiteboardProducer } from './whiteboard.producer';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('WhiteboardProducer', () => {
   let whiteboardProducer: WhiteboardProducer;
+  let client: ClientKafka;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [WhiteboardProducer, ClientKafka],
+      providers: [
+        WhiteboardProducer,
+        {
+          provide: 'CLIENT_KAFKA',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     whiteboardProducer = moduleRef.get<WhiteboardProducer>(WhiteboardProducer);
+    client = moduleRef.get<ClientKafka>('CLIENT_KAFKA');
   });
 
   afterEach(() => {
@@ -19,5 +30,29 @@ describe('WhiteboardProducer', () => {
 
   it('should be defined', () => {
     expect(whiteboardProducer).toBeDefined();
+  });
+
+  describe('sendKafkaMessage', () => {
+    it('should correctly forward the topic name and message payload to the client emit method', () => {
+      const testTopicName = 'testTopic';
+      const testMessage = {
+        context: {
+          tenantId: uuidv4(),
+          casefileId: uuidv4(),
+          eventType: 'testEvent',
+          nodeId: uuidv4(),
+          userId: uuidv4(),
+          userRole: 'admin',
+          timestamp: 123,
+        },
+        body: { test: '123' },
+      };
+      const emitSpy = jest.spyOn(client, 'emit');
+
+      whiteboardProducer.sendKafkaMessage(testTopicName, testMessage);
+
+      expect(emitSpy).toBeCalledTimes(1);
+      expect(emitSpy).toBeCalledWith(testTopicName, testMessage);
+    });
   });
 });
