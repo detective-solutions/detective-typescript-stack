aed58474ffad77c7c995090c6ce63d1e0086e8ce
phips-git <p.mueller93@gmail.com>
2022-06-30 08:35:02 +0200

    test(DET-861): add first batch of gateway tests

--- apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
+++ apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
@@ -1,8 +1,16 @@
+import { IWebSocketClient, WebSocketClientContext } from '../models';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
+import { broadcastWebSocketContext, unicastWebSocketContext } from '../utils';
+
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
+import { WS } from 'jest-websocket-mock';
+import { WebSocket } from 'ws';
 import { WhiteboardProducer } from '../kafka/whiteboard.producer';
 import { WhiteboardWebSocketGateway } from './whiteboard-websocket.gateway';
+import { buildLogContext } from '@detective.solutions/backend/shared/utils';
+import { v4 as uuidv4 } from 'uuid';
 
 const mockWhiteboardProducer = {
   sendKafkaMessage: jest.fn(),
@@ -11,6 +19,10 @@ const mockWhiteboardProducer = {
 const mockJwtService = {};
 
 describe('WhiteboardWebsocketGateway', () => {
+  const webSocketUrl = 'ws://localhost:1234';
+  const testEventType = 'TEST_EVENT';
+  const testMessageBody = { test: 'test' };
+
   let webSocketGateway: WhiteboardWebSocketGateway;
 
   beforeEach(async () => {
@@ -24,13 +36,146 @@ describe('WhiteboardWebsocketGateway', () => {
     }).compile();
 
     webSocketGateway = moduleRef.get<WhiteboardWebSocketGateway>(WhiteboardWebSocketGateway);
+    webSocketGateway.server = new WS(webSocketUrl) as any;
+    webSocketGateway.server.clients = new Set();
   });
 
   afterEach(() => {
+    webSocketGateway.server.close();
     jest.resetAllMocks();
   });
 
   it('should be defined', () => {
     expect(webSocketGateway).toBeDefined();
   });
+
+  describe('sendMessageByContext', () => {
+    const context1 = {
+      tenantId: uuidv4(),
+      casefileId: uuidv4(),
+      nodeId: uuidv4(),
+      userId: uuidv4(),
+      userRole: 'basic',
+      eventType: testEventType,
+      timestamp: 123,
+    };
+    const context2 = {
+      tenantId: uuidv4(),
+      casefileId: uuidv4(),
+      nodeId: uuidv4(),
+      userId: uuidv4(),
+      userRole: 'basic',
+      eventType: testEventType,
+      timestamp: 123,
+    };
+    const context3 = {
+      tenantId: uuidv4(),
+      casefileId: uuidv4(),
+      nodeId: uuidv4(),
+      userId: uuidv4(),
+      userRole: 'basic',
+      eventType: testEventType,
+      timestamp: 123,
+    };
+
+    it('should only broadcast messages to clients with matching context', async () => {
+      const client1 = await createWebSocketClient(context1);
+      const client1Spy = jest.spyOn(client1, 'send').mockImplementation();
+
+      const client2 = await createWebSocketClient(context2);
+      const client2Spy = jest.spyOn(client2, 'send').mockImplementation();
+
+      const client3 = await createWebSocketClient(context2);
+      const client3Spy = jest.spyOn(client3, 'send').mockImplementation();
+
+      const client4 = await createWebSocketClient(context2);
+      const client4Spy = jest.spyOn(client4, 'send').mockImplementation();
+
+      const client5 = await createWebSocketClient(context3);
+      const client5Spy = jest.spyOn(client5, 'send').mockImplementation();
+
+      const testMessage = {
+        context: context2,
+        body: testMessageBody,
+      };
+
+      webSocketGateway.sendMessageByContext(testMessage, broadcastWebSocketContext);
+
+      expect(client1Spy).toBeCalledTimes(0);
+      expect(client2Spy).toBeCalledTimes(1);
+      expect(client3Spy).toBeCalledTimes(1);
+      expect(client4Spy).toBeCalledTimes(1);
+      expect(client5Spy).toBeCalledTimes(0);
+
+      const expectedMessage = JSON.stringify({ event: testEventType, data: testMessage });
+      expect(client2Spy).toBeCalledWith(expectedMessage);
+      expect(client3Spy).toBeCalledWith(expectedMessage);
+      expect(client4Spy).toBeCalledWith(expectedMessage);
+    });
+
+    it('should only unicast messages to single clients with matching context', async () => {
+      const client1Context = { ...context1, userId: 'user1', userRole: 'basic' };
+      const client1 = await createWebSocketClient(client1Context);
+      const client1Spy = jest.spyOn(client1, 'send').mockImplementation();
+
+      const client2Context = { ...context1, userId: 'user2', userRole: 'admin' };
+      const client2 = await createWebSocketClient(client2Context);
+      const client2Spy = jest.spyOn(client2, 'send').mockImplementation();
+
+      const client3Context = { ...context2, userId: 'user3', userRole: 'basic' };
+      const client3 = await createWebSocketClient(client3Context);
+      const client3Spy = jest.spyOn(client3, 'send').mockImplementation();
+
+      const client4Context = { ...context2, userId: 'user4', userRole: 'basic' };
+      const client4 = await createWebSocketClient(client4Context);
+      const client4Spy = jest.spyOn(client4, 'send').mockImplementation();
+
+      const client5Context = { ...context2, userId: 'user4', userRole: 'admin' };
+      const client5 = await createWebSocketClient(client5Context);
+      const client5Spy = jest.spyOn(client5, 'send').mockImplementation();
+
+      const testMessage = {
+        context: { ...context2, userId: 'user4', userRole: 'basic' },
+        body: testMessageBody,
+      };
+
+      webSocketGateway.sendMessageByContext(testMessage, unicastWebSocketContext);
+
+      expect(client1Spy).toBeCalledTimes(0);
+      expect(client2Spy).toBeCalledTimes(0);
+      expect(client3Spy).toBeCalledTimes(0);
+      expect(client4Spy).toBeCalledTimes(1);
+      expect(client5Spy).toBeCalledTimes(0);
+
+      expect(client4Spy).toBeCalledWith(JSON.stringify({ event: testEventType, data: testMessage }));
+    });
+
+    it('should throw an InternalErrorException when no context could be retrieved from a client connection', async () => {
+      const loggerSpy = jest.spyOn(Logger.prototype, 'error');
+      const client = await createWebSocketClient(context1);
+      client._socket.context = undefined;
+
+      const testMessage = {
+        context: context1,
+        body: testMessageBody,
+      };
+
+      expect(() => webSocketGateway.sendMessageByContext(testMessage, unicastWebSocketContext)).toThrow(
+        InternalServerErrorException
+      );
+
+      expect(loggerSpy).toBeCalledTimes(1);
+      expect(loggerSpy).toBeCalledWith(
+        `${buildLogContext(testMessage.context)} Cannot route message, websocket client is missing its context`
+      );
+    });
+  });
+
+  async function createWebSocketClient(clientContext: WebSocketClientContext): Promise<IWebSocketClient> {
+    const client = new WebSocket(webSocketUrl) as IWebSocketClient;
+    client._socket = { context: clientContext };
+    webSocketGateway.server.clients.add(client);
+    await (webSocketGateway.server as any).connected;
+    return client;
+  }
 });
