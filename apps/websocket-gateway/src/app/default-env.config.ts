034287ccf429f04c60fe69ea297228253205cd9b
phips-git <p.mueller93@gmail.com>
2022-07-07 16:21:53 +0200

    build(project): define default ports for services

--- apps/websocket-gateway/src/app/default-env.config.ts
+++ apps/websocket-gateway/src/app/default-env.config.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const defaultEnvConfig = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production').default('development'),
-  ACCESS_TOKEN_SECRET: Joi.string().required(),
+  ACCESS_TOKEN_SECRET: Joi.string().default(7777),
   REFRESH_TOKEN_SECRET: Joi.string().required(), // TODO: Make AuthModule work without specifying this secret
   WEBSOCKET_PORT: Joi.number().required(),
   KAFKA_SERVICE_NAME: Joi.string().required(),
