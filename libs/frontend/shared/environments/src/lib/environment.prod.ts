51b3814ee41afaaf7d43b36eff75abdece8f80b1
phips-git <p.mueller93@gmail.com>
2022-07-26 09:23:57 +0200

    refactor(DET-901): remove comment

--- libs/frontend/shared/environments/src/lib/environment.prod.ts
+++ libs/frontend/shared/environments/src/lib/environment.prod.ts
@@ -2,7 +2,7 @@ import { LogLevel } from '@detective.solutions/frontend/shared/error-handling';
 
 export const environment = {
   production: true,
-  apiBasePath: '', // TODO: Configure /api base path in k8s ingress controller
+  apiBasePath: '',
   webSocketBasePath: '/ws',
   whiteboardUrlPath: '/casefile', // Used to identify whiteboard routes
   authMode: 'custom', // Cannot use AuthMode enum due to circular dependency
