5e816c3c82e35f1749f05d6f44b2f4d3344cce81
phips-git <p.mueller93@gmail.com>
2022-07-30 13:18:39 +0200

    fix(project): fix auth error handling

--- libs/frontend/shared/auth/src/lib/interceptors/auth-http-interceptor.ts
+++ libs/frontend/shared/auth/src/lib/interceptors/auth-http-interceptor.ts
@@ -50,9 +50,9 @@ export class AuthHttpInterceptor implements HttpInterceptor {
   }
 
   isAuthorizationError(error: HttpErrorResponse, request: HttpRequest<any>): boolean {
-    return error instanceof HttpErrorResponse && error.status === 401 && environment.production
-      ? request.url.startsWith(`${window.location.origin}${environment.baseApiPath}`)
-      : request.url.startsWith(`${environment.devApiHost}${environment.baseApiPath}`);
+    return (
+      error instanceof HttpErrorResponse && error.status === 401 && request.url.startsWith(environment.baseApiPath)
+    );
   }
 
   handleAuthorizationError(request: HttpRequest<any>, next: HttpHandler) {
