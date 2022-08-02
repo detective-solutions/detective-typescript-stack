6b497e605228cae3534246edcc6ea3ab7b5b1913
phips-git <p.mueller93@gmail.com>
2022-08-02 17:31:30 +0200

    feat(DET-890): reset apollo cache on login/logout

--- libs/frontend/shared/auth/src/lib/services/auth.custom.service.ts
+++ libs/frontend/shared/auth/src/lib/services/auth.custom.service.ts
@@ -1,11 +1,12 @@
 import { IAuthServerResponse, IJwtTokenPayload, UserRole } from '@detective.solutions/shared/data-access';
+import { Observable, tap } from 'rxjs';
 
 import { $enum } from 'ts-enum-util';
+import { Apollo } from 'apollo-angular';
 import { AuthService } from './auth.service';
 import { HttpClient } from '@angular/common/http';
 import { IAuthStatus } from '../interfaces/auth-status.interface';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from '@detective.solutions/frontend/shared/environments';
 
 // Custom implementation of abstract AuthService to communicate with detective.solutions AuthService backend
@@ -17,19 +18,23 @@ export class CustomAuthService extends AuthService {
   private readonly logoutUrl = `${CustomAuthService.apiPathV1}/logout`;
   private readonly refreshUrl = `${CustomAuthService.apiPathV1}/refresh`;
 
-  constructor(private readonly httpClient: HttpClient) {
+  constructor(private readonly httpClient: HttpClient, private readonly apollo: Apollo) {
     super();
   }
 
   protected loginProvider(email: string, password: string): Observable<IAuthServerResponse> {
-    return this.httpClient.post<IAuthServerResponse>(this.loginUrl, {
-      email,
-      password,
-    });
+    return this.httpClient
+      .post<IAuthServerResponse>(this.loginUrl, {
+        email,
+        password,
+        // Reset Apollo DB cache on login
+      })
+      .pipe(tap(() => this.apollo.client.resetStore()));
   }
 
   protected logoutProvider(): Observable<void> {
-    return this.httpClient.post<void>(this.logoutUrl, {});
+    // Additionally resets Apollo DB cache on logout
+    return this.httpClient.post<void>(this.logoutUrl, {}).pipe(tap(() => this.apollo.client.resetStore()));
   }
 
   protected refreshProvider(): Observable<IAuthServerResponse> {
