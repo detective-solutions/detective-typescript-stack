12d0018572f63b574601fa60139e856bb4afde9b
phips-git <p.mueller93@gmail.com>
2022-08-03 17:54:29 +0200

    feat(DET-902): add effect for loading whiteboard data

--- libs/frontend/whiteboard/src/lib/state/effects/whiteboard-node.effects.ts
+++ libs/frontend/whiteboard/src/lib/state/effects/whiteboard-node.effects.ts
@@ -1,9 +1,12 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { filter, tap } from 'rxjs';
+import { IMessageContext, MessageEventType } from '@detective.solutions/shared/data-access';
+import { combineLatest, filter, of, switchMap, take, tap } from 'rxjs';
 
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { WhiteboardFacadeService } from '../../services';
 import { WhiteboardNodeActions } from '../actions';
+import { selectWhiteboardContextState } from '../selectors';
 
 @Injectable()
 export class WhiteboardNodeEffects {
@@ -19,5 +22,28 @@ export class WhiteboardNodeEffects {
     { dispatch: false }
   );
 
-  constructor(private readonly actions$: Actions, private readonly whiteboardFacade: WhiteboardFacadeService) {}
+  readonly loadWhiteboardData$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(WhiteboardNodeActions.loadWhiteboardData),
+        switchMap((action) =>
+          combineLatest([this.store.select(selectWhiteboardContextState).pipe(take(1)), of(action).pipe(take(1))])
+        ),
+        tap(([context, _action]) => {
+          this.whiteboardFacade.sendWebsocketMessage({
+            event: MessageEventType.LoadWhiteboardData,
+            // TODO: Investigate how to handle empty node ids
+            data: { context: { nodeId: '', ...context } as IMessageContext, body: {} },
+          });
+        })
+      );
+    },
+    { dispatch: false }
+  );
+
+  constructor(
+    private readonly actions$: Actions,
+    private readonly store: Store,
+    private readonly whiteboardFacade: WhiteboardFacadeService
+  ) {}
 }
