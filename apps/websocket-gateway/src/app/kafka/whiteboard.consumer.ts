866d30e6e7eb1ff5e76a2d888f2f4cae7ba9c688
phips-git <p.mueller93@gmail.com>
2022-08-04 15:24:32 +0200

    feat(DET-902): consume uni-/broadcast transaction topics

--- apps/websocket-gateway/src/app/kafka/whiteboard.consumer.ts
+++ apps/websocket-gateway/src/app/kafka/whiteboard.consumer.ts
@@ -1,9 +1,9 @@
 import { Controller, Logger } from '@nestjs/common';
+import { broadcastWebSocketContext, unicastWebSocketContext } from '../utils';
 
 import { EventPattern } from '@nestjs/microservices';
 import { IKafkaMessage } from '@detective.solutions/shared/data-access';
 import { WhiteboardWebSocketGateway } from '../websocket/whiteboard-websocket.gateway';
-import { broadcastWebSocketContext } from '../utils';
 import { buildLogContext } from '@detective.solutions/backend/shared/utils';
 
 @Controller()
@@ -11,6 +11,26 @@ export class WhiteboardConsumer {
   private readonly logger = new Logger(WhiteboardConsumer.name);
 
   constructor(private readonly webSocketGateway: WhiteboardWebSocketGateway) {}
+
+  @EventPattern('transaction_output_unicast')
+  forwardUnicastTransactionOutput(data: IKafkaMessage) {
+    this.logger.verbose(
+      `${buildLogContext(data.value.context)} Consuming unicast message ${data.offset} from topic ${
+        data.topic
+      } with timestamp ${data.timestamp}`
+    );
+    this.webSocketGateway.sendMessageByContext(data.value, unicastWebSocketContext);
+  }
+
+  @EventPattern('transaction_output_broadcast')
+  forwardBroadcastTransactionOutput(data: IKafkaMessage) {
+    this.logger.verbose(
+      `${buildLogContext(data.value.context)} Consuming broadcast message ${data.offset} from topic ${
+        data.topic
+      } with timestamp ${data.timestamp}`
+    );
+    this.webSocketGateway.sendMessageByContext(data.value, broadcastWebSocketContext);
+  }
 
   @EventPattern('casefile')
   forwardQueryExecution(data: IKafkaMessage) {
--- apps/websocket-gateway/src/app/models/event-type-topic-mapping.ts
+++ apps/websocket-gateway/src/app/models/event-type-topic-mapping.ts
@@ -1,6 +1,6 @@
 import { MessageEventType } from '@detective.solutions/shared/data-access';
 
 export const EventTypeTopicMapping = {
-  loadWhiteboardData: { eventType: MessageEventType.LoadWhiteboardData, targetTopic: 'transaction' },
+  loadWhiteboardData: { eventType: MessageEventType.LoadWhiteboardData, targetTopic: 'transaction_input' },
   queryTable: { eventType: MessageEventType.QueryTable, targetTopic: 'masking' },
 };
