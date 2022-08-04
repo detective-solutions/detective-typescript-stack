67714cb85bd7e48d4057efe8a5f8e706b564f886
phips-git <p.mueller93@gmail.com>
2022-08-04 10:03:37 +0200

    refactor(DET-902): clean up app entry point

--- apps/transaction-service/src/main.ts
+++ apps/transaction-service/src/main.ts
@@ -9,9 +9,7 @@ import { microserviceConfig } from './app/microservice-config';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, microserviceConfig as KafkaOptions);
   await app.listen();
-
-  const kafkaPort = app.get(ConfigService).get('KAFKA_PORT');
-  Logger.log(`🚀 Application is running and listening to port ${kafkaPort} (Kafka)`);
+  Logger.log(`🚀 Application is running and listening to port ${app.get(ConfigService).get('KAFKA_PORT')} (Kafka)`);
 }
 
 bootstrap();
