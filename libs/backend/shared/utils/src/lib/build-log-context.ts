992af8774e974983169d4b9d123956f058c1ee5f
phips-git <p.mueller93@gmail.com>
2022-08-05 15:00:55 +0200

    refactor(DET-902): make buildLogContext helper more robust

--- libs/backend/shared/utils/src/lib/build-log-context.ts
+++ libs/backend/shared/utils/src/lib/build-log-context.ts
@@ -1,5 +1,5 @@
 import { IMessageContext } from '@detective.solutions/shared/data-access';
 
 export function buildLogContext(context: IMessageContext): string {
-  return `${context.tenantId}:${context.casefileId}:${context.nodeId}:${context.eventType}:${context.userId} -`;
+  return `${context?.tenantId}:${context?.casefileId}:${context?.nodeId}:${context?.eventType}:${context?.userId} -`;
 }
