4ac303afc56445b9bcd965390a854509d127073d
phips-git <p.mueller93@gmail.com>
2022-08-06 11:04:08 +0200

    feat(DET-902): add backend shared data-access lib

---
+++ libs/backend/shared/data-access/src/lib/messages/message-context.dto.ts
@@ -0,0 +1,37 @@
+import { IMessageContext, MessageEventType, UserRole } from '@detective.solutions/shared/data-access';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, ValidateIf } from 'class-validator';
+
+export class MessageContext implements IMessageContext {
+  @IsEnum(MessageEventType)
+  @IsNotEmpty()
+  eventType: MessageEventType;
+
+  @MaxLength(254)
+  @IsNotEmpty()
+  @IsString()
+  tenantId: string;
+
+  @MaxLength(254)
+  @IsNotEmpty()
+  @IsString()
+  casefileId: string;
+
+  @MaxLength(254)
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsEnum(UserRole)
+  @IsNotEmpty()
+  userRole: UserRole;
+
+  @MaxLength(254)
+  @ValidateIf((nodeId) => nodeId !== '')
+  @IsString()
+  @IsOptional()
+  nodeId: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  timestamp: number;
+}
