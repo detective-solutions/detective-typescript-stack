da5ed7c5946fd673d5b7564ef68f1f3966c359f8
phips-git <p.mueller93@gmail.com>
2022-08-13 13:57:24 +0200

    test(DET-915): log out response data

--- apps/transaction-service/src/app/services/database.service.ts
+++ apps/transaction-service/src/app/services/database.service.ts
@@ -42,6 +42,7 @@ export class DatabaseService {
 
     this.logger.verbose(`Received data for casefile ${id}`);
     const casefileData = response[getCasefileByIdQueryName][0];
+    console.log(casefileData); // TODO: Remove me!
     await validateDto(CasefileForWhiteboard, casefileData, this.logger);
 
     return casefileData;
