70db6114a090b06b0ca61da85fbaeb97257dc6a0
phips-git <p.mueller93@gmail.com>
2022-08-17 08:05:46 +0200

    feat(DET-919): add new message event type

--- libs/shared/data-access/src/lib/messages/message-event-type.enum.ts
+++ libs/shared/data-access/src/lib/messages/message-event-type.enum.ts
@@ -1,5 +1,6 @@
 export enum MessageEventType {
   LoadWhiteboardData = 'LOAD_WHITEBOARD_DATA',
   WhiteboardNodeAdded = 'WHITEBOARD_NODE_ADDED',
+  WhiteboardNodeMoved = 'WHITEBOARD_NODE_MOVED',
   QueryTable = 'QUERY_TABLE',
 }
