1972279a6538075dd76452fb02e71184fc6eef21
phips-git <p.mueller93@gmail.com>
2022-09-12 08:34:47 +0200

    feat(DET-918): handle node deleted messages in ws gateway

--- libs/shared/data-access/src/lib/messages/message-event-type.enum.ts
+++ libs/shared/data-access/src/lib/messages/message-event-type.enum.ts
@@ -1,6 +1,7 @@
 export enum MessageEventType {
   LoadWhiteboardData = 'LOAD_WHITEBOARD_DATA',
   WhiteboardNodeAdded = 'WHITEBOARD_NODE_ADDED',
+  WhiteboardNodeDeleted = 'WHITEBOARD_NODE_DELETED',
   WhiteboardNodeBlocked = 'WHITEBOARD_NODE_BLOCKED',
   WhiteboardNodeMoved = 'WHITEBOARD_NODE_MOVED',
   QueryTable = 'QUERY_TABLE',
