bab1e55c18de9160d4050e8169d08cc7754b129e
phips-git <p.mueller93@gmail.com>
2022-09-16 09:36:07 +0200

    refactor(project): reorder tests

--- apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
+++ apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
@@ -54,29 +54,6 @@ describe('WhiteboardWebsocketGateway', () => {
     expect(webSocketGateway).toBeDefined();
   });
 
-  describe('OnQueryTableEvent', () => {
-    it('should forward QUERY_TABLE events to the correct target topic', async () => {
-      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
-      const testMessage = { context: _createContext(MessageEventType.QueryTable), body: testMessageBody };
-
-      await webSocketGateway.onQueryTableEvent(testMessage);
-
-      expect(producerMock).toBeCalledTimes(1);
-      expect(producerMock).toBeCalledWith(EventTypeTopicMapping.queryTable.targetTopic, testMessage);
-    });
-
-    it('should throw an InternalServerErrorException if the message context validation fails', async () => {
-      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
-      const context = _createContext(MessageEventType.QueryTable);
-      delete context['tenantId']; // tenantId is required in the MessageContextDTO
-
-      expect(webSocketGateway.onQueryTableEvent({ context: context, body: {} })).rejects.toThrow(
-        InternalServerErrorException
-      );
-      expect(producerMock).toBeCalledTimes(0);
-    });
-  });
-
   describe('onLoadWhiteboardDataEvent', () => {
     it('should forward LOAD_WHITEBOARD_DATA events to the correct target topic', async () => {
       const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
@@ -186,6 +163,29 @@ describe('WhiteboardWebsocketGateway', () => {
       delete context['tenantId']; // tenantId is required in the MessageContextDTO
 
       expect(webSocketGateway.onWhiteboardNodeMoved({ context: context, body: {} })).rejects.toThrow(
+        InternalServerErrorException
+      );
+      expect(producerMock).toBeCalledTimes(0);
+    });
+  });
+
+  describe('OnQueryTableEvent', () => {
+    it('should forward QUERY_TABLE events to the correct target topic', async () => {
+      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
+      const testMessage = { context: _createContext(MessageEventType.QueryTable), body: testMessageBody };
+
+      await webSocketGateway.onQueryTableEvent(testMessage);
+
+      expect(producerMock).toBeCalledTimes(1);
+      expect(producerMock).toBeCalledWith(EventTypeTopicMapping.queryTable.targetTopic, testMessage);
+    });
+
+    it('should throw an InternalServerErrorException if the message context validation fails', async () => {
+      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
+      const context = _createContext(MessageEventType.QueryTable);
+      delete context['tenantId']; // tenantId is required in the MessageContextDTO
+
+      expect(webSocketGateway.onQueryTableEvent({ context: context, body: {} })).rejects.toThrow(
         InternalServerErrorException
       );
       expect(producerMock).toBeCalledTimes(0);
