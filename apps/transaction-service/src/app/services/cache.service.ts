a7547672eb0be46f98b6e4bf506100ab71dbe84a
phips-git <p.mueller93@gmail.com>
2022-09-24 14:00:41 +0200

    feat(DET-940): add method to cache service & rename property

--- apps/transaction-service/src/app/services/cache.service.ts
+++ apps/transaction-service/src/app/services/cache.service.ts
@@ -7,11 +7,11 @@ import { RedisClientService } from '@detective.solutions/backend/redis-client';
 export class CacheService {
   readonly logger = new Logger(CacheService.name);
 
-  constructor(private readonly redisService: RedisClientService) {}
+  constructor(private readonly clientService: RedisClientService) {}
 
   async isCasefileCached(casefileId: string): Promise<number> {
     this.logger.verbose(`Checking if casefile ${casefileId} is cached already`);
-    return this.redisService.client.exists(casefileId);
+    return this.clientService.client.exists(casefileId);
   }
 
   async saveCasefile(casefile: ICachedCasefileForWhiteboard): Promise<string> {
@@ -21,7 +21,7 @@ export class CacheService {
     this.logger.log(`Saving casefile ${casefile?.id} to cache`);
     // Can't match expected Redis client type with domain type
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response = await this.redisService.client.json.set(casefile.id, '$', casefile as any);
+    const response = await this.clientService.client.json.set(casefile.id, '.', casefile as any);
 
     if (!response || response !== 'OK') {
       throw new InternalServerErrorException();
@@ -33,6 +33,21 @@ export class CacheService {
     this.logger.log(`Requesting casefile ${casefileId} data from cache`);
     // Can't match Redis client return types with domain type
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return this.redisService.client.json.get(casefileId) as any;
+    return this.clientService.client.json.get(casefileId) as any;
+  }
+
+  async getActiveUsers(casefileId: string) {
+    this.logger.log(`Requesting active connection information for casefile ${casefileId} from cache`);
+    return this.clientService.client.json.get(`.${casefileId}.active-users`);
+  }
+
+  async setActiveUser(userId: string, casefileId: string) {
+    this.logger.log(`Adding active user ${userId} to casefile ${casefileId}`);
+    const response = await this.clientService.client.json.set(casefileId, '.active-users', userId);
+
+    if (!response || response !== 'OK') {
+      throw new InternalServerErrorException();
+    }
+    return response;
   }
 }
