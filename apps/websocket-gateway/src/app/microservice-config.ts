85b7b00473b4607c91b42a7a9b28e13c60622243
phips-git <p.mueller93@gmail.com>
2022-09-24 13:52:23 +0200

    build(project): make gateway consumer group id configurable

--- apps/websocket-gateway/src/app/microservice-config.ts
+++ apps/websocket-gateway/src/app/microservice-config.ts
@@ -13,7 +13,11 @@ export const microserviceConfig = {
       },
     },
     consumer: {
-      groupId: 'websocket-gateway-consumer',
+      groupId:
+        // Currently, each gateway needs to be in its own consumer group!
+        // This way it can be ensured that each gateway consumes all incoming events and has the chance to forward
+        // them to connected webclients within an applicable context
+        process.env.KAFKA_CONSUMER_GROUP_ID ?? `websocket-gateway-consumer-group-${Math.floor(Math.random() * 12)}`,
     },
   },
 };
