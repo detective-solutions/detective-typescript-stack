6e648b68dba60cbe320ee3e1652a0db164f67728
phips-git <p.mueller93@gmail.com>
2022-10-05 07:42:39 +0200

    feat(DET-940): update ICasefileForWhiteboard interface

--- libs/shared/data-access/src/lib/casefile/casefile-for-whiteboard.interface.ts
+++ libs/shared/data-access/src/lib/casefile/casefile-for-whiteboard.interface.ts
@@ -1,3 +1,6 @@
 import { ICasefile } from './casefile.interface';
 
-export type ICasefileForWhiteboard = Pick<ICasefile, 'id' | 'title' | 'description' | 'tables' | 'embeddings'>;
+export type ICasefileForWhiteboard = Pick<
+  ICasefile,
+  'id' | 'title' | 'description' | 'tables' | 'queries' | 'embeddings'
+>;
