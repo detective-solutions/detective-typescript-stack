74301b236f9301adc817bb5c76647b8830cedb71
phips-git <p.mueller93@gmail.com>
2022-10-09 10:17:04 +0200

    fix(DET-940): remove unnecessary DTO classes

--- libs/backend/shared/data-access/src/lib/table/index.ts
+++ libs/backend/shared/data-access/src/lib/table/index.ts
@@ -1,2 +1,1 @@
-export * from './table-occurrence-input.dto';
 export * from './table-occurrence.dto';
--- libs/backend/shared/data-access/src/lib/user-query/index.ts
+++ libs/backend/shared/data-access/src/lib/user-query/index.ts
@@ -1,2 +1,1 @@
-export * from './user-query-occurrence-input.dto';
 export * from './user-query-occurrence.dto';
