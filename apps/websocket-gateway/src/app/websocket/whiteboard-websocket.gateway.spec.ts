acfae1fa3afa5305b0a65000acd7937b7315d4ed
phips-git <p.mueller93@gmail.com>
2022-11-20 13:25:41 +0100

    test(DET-967): add node resizing ws gateway unit test

--- apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
+++ apps/websocket-gateway/src/app/websocket/whiteboard-websocket.gateway.spec.ts
@@ -193,6 +193,29 @@ describe('WhiteboardWebsocketGateway', () => {
     });
   });
 
+  describe('onWhiteboardNodeResizedEvent', () => {
+    it('should forward WHITEBOARD_NODE_RESIZED events to the correct target topic', async () => {
+      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
+      const testMessage = { context: _createContext(MessageEventType.WhiteboardNodeResized), body: testMessageBody };
+
+      await webSocketGateway.onWhiteboardNodeResized(testMessage);
+
+      expect(producerMock).toBeCalledTimes(1);
+      expect(producerMock).toBeCalledWith(EventTypeTopicMapping.whiteboardNodeResized.targetTopic, testMessage);
+    });
+
+    it('should throw an InternalServerErrorException if the message context validation fails', async () => {
+      const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
+      const context = _createContext(MessageEventType.WhiteboardNodeTitleUpdated);
+      delete context['tenantId']; // tenantId is required in the MessageContextDTO
+
+      expect(webSocketGateway.onWhiteboardNodeResized({ context: context, body: {} })).rejects.toThrow(
+        InternalServerErrorException
+      );
+      expect(producerMock).toBeCalledTimes(0);
+    });
+  });
+
   describe('onWhiteboardTitleUpdatedEvent', () => {
     it('should forward WHITEBOARD_TITLE_UPDATED events to the correct target topic', async () => {
       const producerMock = jest.spyOn(mockWhiteboardProducer, sendKafkaMessageMethodName);
