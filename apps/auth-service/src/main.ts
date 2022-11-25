9d58c94c7ef1cf23c00676a831428073c71e8ed3
phips-git <p.mueller93@gmail.com>
2022-11-25 11:29:27 +0100

    ci(DET-1014): provoke service builds

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -6,7 +6,6 @@ import { NestFactory } from '@nestjs/core';
 import { environment } from '@detective.solutions/backend/shared/environments';
 
 async function bootstrap() {
-  console.log(); // TODO: REMOVE ME
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
 
   app.enableVersioning({
