dccda7c7fb05bad4df37f5b93a5030625e388f94
phips-git <p.mueller93@gmail.com>
2022-11-27 01:59:06 +0100

    ci(DET-1014): provoke service build

--- apps/detective-client/src/main.ts
+++ apps/detective-client/src/main.ts
@@ -3,7 +3,6 @@ import { enableProdMode } from '@angular/core';
 import { environment } from '@detective.solutions/frontend/shared/environments';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-console.log(); // TODO: REMOVE ME!
 if (environment.production) {
   enableProdMode();
 }
