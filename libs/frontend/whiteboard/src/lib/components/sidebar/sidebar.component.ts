96bb5c0186342180fb4e7f6bba0d79bc3c448869
phips-git <p.mueller93@gmail.com>
2022-12-12 08:13:31 +0100

    feat(DET-864): enhance sidebar assets menu

--- libs/frontend/whiteboard/src/lib/components/sidebar/sidebar.component.ts
+++ libs/frontend/whiteboard/src/lib/components/sidebar/sidebar.component.ts
@@ -1,16 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  OnInit,
-  QueryList,
-  ViewChild,
-  ViewChildren,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { GetAllWhiteboardTablesGQL, IGetAllWhiteboardTablesGQLResponse } from '../../graphql';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, Subscription, map } from 'rxjs';
 
+import { MatMenuTrigger } from '@angular/material/menu';
 import { QueryRef } from 'apollo-angular';
 import { SourceConnectionDTO } from '@detective.solutions/frontend/shared/data-access';
 import { WhiteboardNodeType } from '@detective.solutions/shared/data-access';
@@ -19,48 +11,40 @@ import { WhiteboardNodeType } from '@detective.solutions/shared/data-access';
   selector: 'whiteboard-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  @ViewChild('assetsMenuTrigger') assetsMenuTrigger!: MatMenuTrigger;
   @ViewChildren('tableOccurrence', { read: ElementRef }) tableOccurrence!: QueryList<ElementRef>;
   @ViewChild('embedding', { read: ElementRef }) embedding!: ElementRef;
-  @ViewChild('dataSourceMenuIcon', { read: ElementRef }) dataSourceMenuIcon!: ElementRef;
+  @ViewChild('assetsMenuIcon', { read: ElementRef }) assetsMenuIcon!: ElementRef;
 
-  dataSources$!: Observable<any[]>;
+  dataSources!: any[];
+
+  isLoading = false;
+  dataSourcesLoaded = false;
 
   private getAllConnectionsWatchQuery!: QueryRef<Response>;
+  private readonly subscriptions = new Subscription();
 
   constructor(private readonly getAllSourceConnectionsGQL: GetAllWhiteboardTablesGQL) {}
 
-  ngOnInit() {
-    this.dataSources$ = this.getAllConnections(0, 1000).pipe(
-      map((response: SourceConnectionDTO[]) => {
-        const connectedTables = response.map((sourceConnection: SourceConnectionDTO) => {
-          return sourceConnection.connectedTables.map((connectedTable: any) => {
-            return {
-              ...connectedTable,
-              iconSrc: sourceConnection.iconSrc,
-            };
-          });
-        });
-        return connectedTables.flat();
-      }),
-      tap(console.log)
-    );
-  }
-
-  onMenuOpened() {
-    console.log('YO');
+  closeDataSourceMenu() {
+    this.assetsMenuTrigger.closeMenu();
   }
 
   onDragStart(event: DragEvent) {
     const isTable = this.tableOccurrence.find((elementRef: ElementRef) => elementRef.nativeElement === event.target);
     if (isTable) {
-      event.dataTransfer?.setDragImage(this.dataSourceMenuIcon.nativeElement, 0, 0);
+      event.dataTransfer?.setDragImage(this.assetsMenuIcon.nativeElement, 0, 0);
+      console.log(isTable.nativeElement.dataset);
       event.dataTransfer?.setData(
         'text/plain',
-        JSON.stringify({ type: WhiteboardNodeType.TABLE, entityId: isTable.nativeElement.id })
+        JSON.stringify({
+          type: WhiteboardNodeType.TABLE,
+          entityId: isTable.nativeElement.dataset.entityId,
+          title: isTable.nativeElement.dataset.name,
+        })
       );
       return;
     }
@@ -68,6 +52,45 @@ export class SidebarComponent implements OnInit {
     const isEmbedding = event.target === this.embedding.nativeElement;
     if (isEmbedding) {
       event.dataTransfer?.setData('text/plain', JSON.stringify({ type: WhiteboardNodeType.EMBEDDING }));
+    }
+  }
+
+  onScroll(event: Event) {
+    console.log(event);
+  }
+
+  getDataSources() {
+    if (!this.dataSourcesLoaded) {
+      this.isLoading = true;
+      this.subscriptions.add(
+        this.getAllSourceConnectionsGQL
+          .fetch({ paginationOffset: 0, pageSize: 20 })
+          .pipe(
+            map((response: any) => response.data),
+            map((response: IGetAllWhiteboardTablesGQLResponse) =>
+              response.querySourceConnection.map(SourceConnectionDTO.Build)
+            ),
+            map((response: SourceConnectionDTO[]) =>
+              response.filter((sourceConnection: SourceConnectionDTO) => sourceConnection.connectedTables.length > 0)
+            ),
+            map((response: SourceConnectionDTO[]) => {
+              const connectedTables = response.map((sourceConnection: SourceConnectionDTO) => {
+                return sourceConnection.connectedTables.map((connectedTable: any) => {
+                  return {
+                    ...connectedTable,
+                    iconSrc: sourceConnection.iconSrc,
+                  };
+                });
+              });
+              return connectedTables.flat();
+            })
+          )
+          .subscribe((response: object[]) => {
+            this.isLoading = false;
+            this.dataSources = response;
+            this.dataSourcesLoaded = true;
+          })
+      );
     }
   }
 
--- libs/frontend/whiteboard/src/lib/whiteboard.material.module.ts
+++ libs/frontend/whiteboard/src/lib/whiteboard.material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 
@@ -13,6 +14,7 @@ const modules = [
   MatTooltipModule,
   MatMenuModule,
   MatInputModule,
+  MatProgressSpinnerModule,
   DragDropModule,
   FlexLayoutModule,
 ];
