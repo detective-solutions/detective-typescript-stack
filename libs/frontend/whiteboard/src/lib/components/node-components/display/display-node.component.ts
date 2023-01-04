009fb9b6312f8dbe1cdce22239faab24c4de6b1e
Maxim Perl <maxim.perl@outlook.de>
2023-01-04 16:20:09 +0100

    build(DET-999):add disable sort import

--- libs/frontend/whiteboard/src/lib/components/node-components/display/display-node.component.ts
+++ libs/frontend/whiteboard/src/lib/components/node-components/display/display-node.component.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/import-sort */
+/* eslint-disable sort-imports */
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { IDisplayWhiteboardNode } from '@detective.solutions/shared/data-access';
 import { BaseNodeComponent } from '../base/base-node.component';
