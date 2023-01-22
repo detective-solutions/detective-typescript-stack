ca12905aaca6eaf8c23b45ba5187d8cccc91ef04
phips-git <p.mueller93@gmail.com>
2023-01-22 15:04:09 +0100

    refactor(DET-1032): remove unused import

--- libs/backend/shared/data-access/src/lib/embedding/embedding.dto.ts
+++ libs/backend/shared/data-access/src/lib/embedding/embedding.dto.ts
@@ -1,5 +1,6 @@
-import { IEmbedding, IUser } from '@detective.solutions/shared/data-access';
 import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
+
+import { IEmbedding } from '@detective.solutions/shared/data-access';
 
 export class EmbeddingDTO implements IEmbedding {
   @IsUUID()
