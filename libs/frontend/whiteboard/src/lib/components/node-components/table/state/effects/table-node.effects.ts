f66ff3282a7923058ba521594886e5745da405a5
phips-git <p.mueller93@gmail.com>
2023-03-24 07:31:25 +0100

    feat(DET-1045): reactivate table query

--- libs/frontend/whiteboard/src/lib/components/node-components/table/state/effects/table-node.effects.ts
+++ libs/frontend/whiteboard/src/lib/components/node-components/table/state/effects/table-node.effects.ts
@@ -1,9 +1,12 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { combineLatest, of, switchMap, take } from 'rxjs';
+import { IMessageContext, MessageEventType } from '@detective.solutions/shared/data-access';
+import { IQueryMessagePayload, QueryType } from '../../models';
+import { combineLatest, of, switchMap, take, tap } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TableNodeActions } from '../actions';
+import { WhiteboardFacadeService } from '../../../../../services/whiteboard-facade.service';
 import { selectWhiteboardContextState } from '../../../../../state';
 
 @Injectable()
@@ -14,28 +17,32 @@ export class TableNodeEffects {
         ofType(TableNodeActions.LoadTableData),
         switchMap((action) =>
           combineLatest([this.store.select(selectWhiteboardContextState).pipe(take(1)), of(action).pipe(take(1))])
+        ),
+        tap(([context, action]) =>
+          this.whiteboardFacade.sendWebsocketMessage({
+            event: MessageEventType.QueryTable,
+            data: {
+              context: {
+                ...context,
+                eventType: MessageEventType.QueryTable,
+                nodeId: action.node.id,
+              } as IMessageContext,
+              body: {
+                queryType: QueryType.SqlQuery,
+                query: [action.node.entity.baseQuery],
+                tableId: [action.node.entity.id],
+              } as IQueryMessagePayload,
+            },
+          })
         )
-        // tap(([context, action]) =>
-        //   this.whiteboardFacade.sendWebsocketMessage({
-        //     event: MessageEventType.QueryTable,
-        //     data: {
-        //       context: {
-        //         ...context,
-        //         eventType: MessageEventType.QueryTable,
-        //         nodeId: action.node.id,
-        //       } as IMessageContext,
-        //       body: {
-        //         queryType: QueryType.SqlQuery,
-        //         query: ['SELECT * FROM freequery LIMIT 100'],
-        //         tableId: ['59c9547a-dea7-11ec-ac54-287fcf6e439d'],
-        //       } as IQueryMessagePayload,
-        //     },
-        //   })
-        // )
       );
     },
     { dispatch: false }
   );
 
-  constructor(private readonly actions$: Actions, private readonly store: Store) {}
+  constructor(
+    private readonly actions$: Actions,
+    private readonly store: Store,
+    private readonly whiteboardFacade: WhiteboardFacadeService
+  ) {}
 }
